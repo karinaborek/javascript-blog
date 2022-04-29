d2d42819d52825f1a1e479fce90a1d1a57e64da9
Karolina <karolina.blaszyk.bd.pl@gmail.com>
2022-04-29 05:42:09 +0200

    Add class active

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
+    
   
-    /* remove class 'active' from all article links  */
-
+    /* [DONE] remove class 'active' from all article links  */
+  
     const activeLinks = document.querySelectorAll('.titles a.active');
-
-        for(let activeLink of activeLinks){
-            activeLink.classList.remove('active');
-}
   
-    /* add class 'active' to the clicked link */
-
-    const activeArticles = document.querySelectorAll('.titles articles.active');
-
-        for(let activeArticle of activeArticles){
-            activeArticle.classList.remove('active');
-}
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
   
-    /* remove class 'active' from all articles */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+  
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
+    
+  
+    /* [DONE] remove class 'active' from all articles */
   
     /* get 'href' attribute from the clicked link */
   
