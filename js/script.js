0ffa75ac060f89853af33d06b5c165b30446b7da
Karolina <karolina.blaszyk.bd.pl@gmail.com>
2022-05-04 09:03:20 +0200

    Modified script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     
@@ -14,17 +15,34 @@ const titleClickHandler = function(event){
     /* [IN PROGRESS] add class 'active' to the clicked link */
   
     clickedElement.classList.add('active');
-    console.log('clickedElement:', clickedElement);
+    console.log('clickedElement', clickedElement);
     
   
     /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.titles articles.active');
+  
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
+
+    const articleSelector = document.querySelectorAll('a.href');
+    const a = clickedElement.getAttribute('href');
+    console.log('articleSelector');
+
   
     /* find the correct article using the selector (value of 'href' attribute) */
   
+    const targetArticle = document.querySelector('a.href');
+    console.log('targetArticle');
+
     /* add class 'active' to the correct article */
-  }
+
+    targetArticle.classList.add('active');
+    console.log('targetArticle', targetArticle);
+  
   
   const links = document.querySelectorAll('.titles a');
   
